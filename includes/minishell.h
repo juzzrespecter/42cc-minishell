b74dd5c80bc18213f17f161e231ffa83c645432d
Daniel Rodriguez pueyo <danrodri@c1r9s2.42madrid.com>
2021-02-23 19:23:53 +0100

    ...

---
+++ includes/minishell.h
@@ -0,0 +1,19 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+typedef struct s_minishell {
+	char **argv;
+
+}	t_minishell;
+
+# include "libft.h"
+# include <sys/stat.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <dirent.h>
+# include <signal.h>
+# include <errno.h>
+
+# endif
---
+++ srcs/main.c
@@ -0,0 +1,14 @@
+#include "minishell.h" 
+
+int main(int argc, char **argv, char **envp)
+{
+	/* variables a inicializar */
+
+	while ( /* condicion */ )
+	{
+		/* imprime 
+		 * read();
+		 */
+	}
+	return ();
+}
