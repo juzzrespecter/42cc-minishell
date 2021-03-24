e4402542ac160515cbf0b34049aa20bf1aa71ec0
Daniel Rodriguez pueyo <danrodri@c1r9s2.42madrid.com>
2021-03-24 19:03:38 +0100

    cd & echo

--- b_cd.c
+++ b_cd.c
@@ -2,25 +2,27 @@
 
 int	b_cd(char **argv, t_data *data)
 {
-	char	*path;                                                              /* path al directorio de destino                                */
-	char	*home;                                                              /* variable de entorno HOME                                     */
-	char	*pwd;                                                               /* variable de entorno PWD (se actualiza con cada llamada a cd  */
-	char	*id;                                                                /* id: string en el array de strings env para actualizar PWD    */
+	char	*path;
+	char	*home;
+	char	*pwd;
+	char	*id;
 	int		export_pwd_ret;
 
 	path = argv[1];
-	if (path == NULL)                                                           /* si cd se ejecuta sin argumento, usa la variable de entorno HOME  */
+	if (path == NULL)
 	{
 		if ((home = search_env(data->env, "HOME=")) == NULL)
 		       return (print_error(argv[1], "HOME not set")); /* sin argumento y sin HOME, cd lanza error y sale con 1            */
-		path = home + 5;                                                        /* desplaza el puntero a HOME hasta el inicio del path              */ 
+		path = home + 5; 
 	}
-	if (chdir(path) == -1 || (pwd = getcwd(NULL, 0)) == NULL)                   /* lanza chdir e intenta recibir el nuevo path absoluto por getcwd  */
+	if (chdir(path) == -1 || (pwd = getcwd(NULL, 0)) == NULL)
 		return (print_error(argv[0], strerror(errno)));
-	if ((id = ft_strjoin("PWD=", pwd)) == NULL)                                 /* monta el id (name=value) para guardarlo en env                   */
+	if ((id = ft_strjoin("PWD=", pwd)) == NULL)
 		return (print_error(argv[0], strerror(errno)));
-	free(pwd);
 	export_pwd_ret = env_add_id(id, data);
+	free(data->pwd);
+	data->pwd = ft_strdup(pwd);
+	free(pwd);
 	free(id);
 	return (export_pwd_ret);
 }
--- b_echo.c
+++ b_echo.c
@@ -2,21 +2,24 @@
 
 int		b_echo(char **argv)
 {
-	int		n_flag;					/* flag -n: no imprime salto de linea al final de la cadena  */
+	int		n_flag;
 	int		i;
 
 	n_flag = 0;
 	i = 0;
 	if (argv[1] != NULL)
-		if (ft_strncmp(argv[1], "-n", ft_strlen(argv[1])) == 0)	/* solo toma en cuenta el flag si es el primer argumento del comando	*/
+		if (ft_strncmp(argv[1], "-n", ft_strlen(argv[1])) == 0)
+		{
+			i++;
 			n_flag = 1;
+		}
 	while (argv[i + 1] != NULL)
 	{
-		write(1, argv[i + 1], ft_strlen(argv[i + 1]));		/* imprime	en fd de output	*/
+		write(1, argv[i + 1], ft_strlen(argv[i + 1]));
 		if (argv[i + 2] != NULL)
 			write(1, " ", 1);
 		else
-			if (n_flag == 0)									/* ignora el salto de linea si flag -n == true	*/
+			if (n_flag == 0)						
 				write(1, "\n", 1);
 		i++;
 	}
