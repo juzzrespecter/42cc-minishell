fc4c94bde67aa98b61d6a4a891f321f2490f56dc
Dani <danisarp@gmail.com>
2021-05-10 19:44:00 +0200

    export: imprime las variables de entorno en orden alfabetico.

--- b_export.c
+++ b_export.c
@@ -1,15 +1,58 @@
 #include "minishell.h"
 
+static void	sort_env_cmp(char **env_sorted, int pos)
+{
+	int	cmp_out;
+	int	len_env;
+	char	*str_aux;
+
+	len_env = ft_strlen(env_sorted[pos]) + 1;
+	cmp_out = ft_strncmp(env_sorted[pos], env_sorted[pos + 1], len_env);
+	if (cmp_out > 0)
+	{
+		str_aux = env_sorted[pos];
+		env_sorted[pos] = env_sorted[pos + 1];
+		env_sorted[pos + 1] = str_aux;
+	}
+}	
+
+static char	**sort_env(char **env)
+{
+	char **env_sorted;
+	int	env_len;
+	int	i;
+	int	j;
+
+	env_sorted = copy_env(env);
+	env_len = envlen(env_sorted);
+	i = 0;
+	j = 0;
+	while (i < env_len)
+	{
+		while (env_sorted[j])
+		{
+			if (env_sorted[j + 1] != NULL)
+				sort_env_cmp(env_sorted, j);
+			j++;	
+		}
+		j = 0;
+		i++;
+	}
+	return (env_sorted);
+}
+
 static int	export_print(t_data *data)
 {
 	int		i;
 	int		j;
 	char	*value;
+	char **env_sorted;
 
+	env_sorted = sort_env(data->env);
 	i = 0;
-	while (data->env[i])
+	while (env_sorted[i])
 	{
-		value = data->env[i];
+		value = env_sorted[i];
 		j = 0;
 		while (value[j] != '=' && value[j] != 0)
 			j++;
@@ -20,6 +63,7 @@ static int	export_print(t_data *data)
 		write(1, "\"\n", 2);
 		i++;
 	}
+	free_env(env_sorted);
 	return (0);
 }
 
